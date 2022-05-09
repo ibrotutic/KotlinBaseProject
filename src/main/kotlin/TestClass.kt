df9346623466dcf323b3fdf78720270aff9bdfaf
Ibro Tutic <tutici@iastate.edu>
2022-05-09 10:36:51 -0500

    "Mock libs and additional test classes"

--- src/main/kotlin/TestClass.kt
+++ src/main/kotlin/TestClass.kt
@@ -1,7 +1,27 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.coroutines.CoroutineContext
+
 class TestClass(
     private val initialAmount: Int
-) {
+): CoroutineScope  {
+
+    val flow = MutableStateFlow(false)
+
+    init {
+        launch {
+            withContext(Dispatchers.IO) {
+                delay(5000)
+                flow.emit(true)
+                System.out.println("Emitting")
+            }
+        }
+    }
+
     fun getInitial(): Int {
         return initialAmount
     }
+
+    override val coroutineContext: CoroutineContext
+        get() = SupervisorJob()
 }
--- src/test/kotlin/TestClassTest.kt
+++ src/test/kotlin/TestClassTest.kt
@@ -1,11 +1,24 @@
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class TestClassTest {
+
+
+    private val mockInterface = mockk<TestInterface>()
     private val validator = TestClass(5)
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun test_valid() {
-        assertEquals(5, validator.getInitial())
+    fun test_valid() = runTest {
+
+        every { mockInterface.hello() } returns "nothing"
+
+        assertEquals("nothing", mockInterface.hello())
     }
 }
